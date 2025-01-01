dcf9f69000d6992a0514bb84d416824ad5ff48e7
jeka <jekagost@gmail.com>
2025-01-02 00:02:39 +0300

    logOutButton added to CarsLibraryController

--- src/main/java/com/example/cars_rent/CarsLibraryController.java
+++ src/main/java/com/example/cars_rent/CarsLibraryController.java
@@ -3,12 +3,16 @@ package com.example.cars_rent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 import javafx.scene.control.Label;
 
 public class CarsLibraryController {
+
+    @FXML
+    private Button logOutButton;
 
     @FXML
     private void onLogOutButtonClıck() throws IOException {
