2be1ddd7fa0336b8ea176f15ce5f55cabd32027d
jeka <jekagost@gmail.com>
2025-01-01 23:48:03 +0300

    try to chek-in V4 & solved 4 problems (deleted car-card.fxml and main-view.fxml)

--- src/main/java/com/example/cars_rent/CarsLibraryController.java
+++ src/main/java/com/example/cars_rent/CarsLibraryController.java
@@ -9,4 +9,13 @@ import java.io.IOException;
 import javafx.scene.control.Label;
 
 public class CarsLibraryController {
+
+    @FXML
+    private void onLogOutButtonClıck() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Scene secondScene = new Scene(loader.load());
+
+        Stage stage = (Stage) Stage.getWindows().filtered(window -> window.isShowing()).get(0);
+        stage.setScene(secondScene);
+    }
 }
