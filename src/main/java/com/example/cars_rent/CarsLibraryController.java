2f17650f5839aa593ae8d39d1d301310cb99dc6f
jeka <jekagost@gmail.com>
2025-01-03 14:31:10 +0300

    Day update V6 Admim cars button fix

--- src/main/java/com/example/cars_rent/CarsLibraryController.java
+++ src/main/java/com/example/cars_rent/CarsLibraryController.java
@@ -27,10 +27,10 @@ public class CarsLibraryController {
         openFilteredCarsWindow(false);
     }
 
-    @FXML
+   /* @FXML
     private void showAllCars() {
-        openFilteredCarsWindow(null);
-    }
+        openFilteredCarsWindow(true);
+    } */
 
 
     /*@FXML
