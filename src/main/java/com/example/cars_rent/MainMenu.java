d1e996d7b32ce7d6bafba22f6072cadaaa0de3c1
jeka <jekagost@gmail.com>
2025-01-03 03:15:55 +0300

    Night Update V7

--- src/main/java/com/example/cars_rent/MainMenu.java
+++ src/main/java/com/example/cars_rent/MainMenu.java
@@ -1,4 +1,18 @@
 package com.example.cars_rent;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainMenu {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RegisterPage.class.getResource("register-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 880, 900);
+        stage.setTitle("Register Page");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
